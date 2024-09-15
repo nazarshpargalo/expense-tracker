72b0bc2223e9eaf2f2109e05141149f3cea76bcb
Nazarii Shparhalo <nazariishparhalo@Nazariis-MacBook-Pro.local>
2024-09-15 22:19:56 +0300

    add logs and changes docke rcompose

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,14 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  console.log({
+    port: process.env.DATABASE_PORT,
+    host: process.env.DATABASE_HOST,
+    pass: process.env.DATABASE_PASSWORD,
+    username: process.env.DATABASE_USERNAME,
+    db_name: process.env.DATABASE_NAME,
+  });
+
   await app.listen(3000);
 }
 bootstrap();
